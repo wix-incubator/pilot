8d3cde74e00ad6422ca7c46fc20a67739d56137c
Asaf Korem <asaf.korem@gmail.com>
2025-03-07 21:39:25 +0200

    refactor(types): rename `cache` to `common`.

--- packages/core/src/types/index.ts
+++ packages/core/src/types/index.ts
@@ -2,5 +2,5 @@ export * from "@/types/core";
 export * from "@/types/framework";
 export * from "@/types/prompt";
 export * from "@/types/auto";
-export * from "@/types/cache";
+export * from "@/types/common";
 export * from "@/types/logger";
