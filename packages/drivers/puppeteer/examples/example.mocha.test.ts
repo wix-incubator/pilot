977ce82716fc324f15039b1cbf3807684b2c77fa
lironsh <lironsh@wix.com>
2025-06-18 17:35:20 +0300

    clean mocha and vitest and fix jest.config

--- packages/drivers/puppeteer/examples/example.mocha.test.ts
+++ packages/drivers/puppeteer/examples/example.mocha.test.ts
@@ -47,49 +47,6 @@ describe("Example Test Suite", function () {
       }
     };
 
-  it.skip("perform test with pilot", async function () {
-    await pilot.perform(
-      "Open https://www.wix.com/domains with GUI browser",
-      "Tap on the domain search input",
-      "Type wix-pilot.com in the domain search input",
-      "Tap on the Search button",
-      "The searched domain availability message appears",
-      'Click on the "Get it" button',
-      "Chose 5 years plan",
-      'Press on the "Continue" button',
-      "Fill the form with the following details: First Name: John, Last Name: Doe, Email: JohnDoe@gmail.com, Phone: 1234567890, Address: 1234 Wix Ave, City: San Francisco, State: California, Zip: 12345",
-      'Press on the "Continue" button',
-      'Select "Private Registration" option',
-      "Verify the total price is including the domain and the private registration",
-      "Continue",
-      "Verify you are in the checkout page",
-    );
-  });
-
-  it.skip("perform test with pilot", async function () {
-    await pilot.perform(
-      "Open https://www.wix.com/domains with GUI browser",
-      "Tap on the domain search input",
-      "Type wix-pilot.com in the domain search input",
-      "Tap on the Search button",
-    );
-  });
-
-  it.skip("perform test with autopilot", async function () {
-    await pilot.autopilot(
-      "Open https://www.wix.com/domains and check domain availability for wix-pilot.net with GUI browser",
-    );
-  });
-
-  it.skip(
-    "auto perform add to cart",
-    wrapTest(async function () {
-      await pilot.autopilot(
-        "Open https://www.yohaiknaani.com, go to `Shop All` page and add `digital varient` to the cart",
-      );
-    }),
-  );
-
   it.only("open a site", async function () {
     await pilot.perform("Open https://www.yohaiknaani.com");
   });
