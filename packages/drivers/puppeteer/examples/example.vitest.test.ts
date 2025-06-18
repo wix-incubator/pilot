aabe396e067caf78653b0645e85b893452c5b37e
lironsh <lironsh@wix.com>
2025-06-18 17:35:20 +0300

    clean mocha and vitest and fix jest.config

--- packages/drivers/puppeteer/examples/example.vitest.test.ts
+++ packages/drivers/puppeteer/examples/example.vitest.test.ts
@@ -42,40 +42,6 @@ describe("Example Test Suite", () => {
     pilot.end(!testFailed);
   });
 
-  it.skip("perform test with pilot", async () => {
-    await pilot.perform(
-      "Open https://www.wix.com/domains with GUI browser",
-      "Tap on the domain search input",
-      "Type wix-pilot.com in the domain search input",
-      "Tap on the Search button",
-      "The searched domain availability message appears",
-      'Click on the "Get it" button',
-      "Chose 5 years plan",
-      'Press on the "Continue" button',
-      "Fill the form with the following details: First Name: John, Last Name: Doe, Email: JohnDoe@gmail.com, Phone: 1234567890, Address: 1234 Wix Ave, City: San Francisco, State: California, Zip: 12345",
-      'Press on the "Continue" button',
-      'Select "Private Registration" option',
-      "Verify the total price is including the domain and the private registration",
-      "Continue",
-      "Verify you are in the checkout page",
-    );
-  }, 300000);
-
-  it.skip("perform test with pilot", async () => {
-    await pilot.perform(
-      "Open https://www.wix.com/domains with GUI browser",
-      "Tap on the domain search input",
-      "Type wix-pilot.com in the domain search input",
-      "Tap on the Search button",
-    );
-  }, 300000);
-
-  it.skip("perform test with autopilot", async () => {
-    await pilot.autopilot(
-      "Open https://www.wix.com/domains and check domain availability for wix-pilot.net with GUI browser",
-    );
-  }, 300000);
-
   it.only("open a site", async () => {
     await pilot.perform("Open https://www.yohaiknaani.com site");
   }, 300000);
