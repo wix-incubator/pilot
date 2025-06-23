2b835329c5ce665b2c6f630a98747128a138efeb
lironsh <lironsh@wix.com>
2025-06-23 09:32:26 +0300

    fix: add time stamp and temp directory

--- packages/drivers/appium/utils/findTextLocation.ts
+++ packages/drivers/appium/utils/findTextLocation.ts
@@ -1,12 +1,14 @@
 import path from "path";
 import {writeFileSync} from "fs";
 import {getTextCoordinates} from "./getTextCoordinates";
+import os from "os";
 
 async function findTextLocation (word: string) {
     const base64Image = await driver.takeScreenshot();
     if (!base64Image) throw new Error("Failed to capture screenshot");
 
-    const tempImagePath = path.join(__dirname, "temp_screenshot.png");
+    const timestamp = Date.now()
+    const tempImagePath = path.join(os.tmpdir(), `temp_screenshot_${timestamp}.png`);
     writeFileSync(tempImagePath, base64Image, { encoding: "base64" });
 
     const screenSize = await driver.getWindowSize();
